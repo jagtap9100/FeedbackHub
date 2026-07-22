f437e36976da38685691344eeca5dad6168a07e8
jagtap9100 <Abhishek.Wdeveloper@gmail.com>
2026-07-22 18:19:27 +0530

    Configure the same variables in Vercel

--- app/api/feedback/route.ts
+++ app/api/feedback/route.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 import { NextResponse } from "next/server";
 
-const supabaseUrl = process.env.SUPABASE_URL || "";
-const supabaseAnonKey = process.env.SUPABASE_PUBLISHABLE_KEY || "";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY || "";
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
